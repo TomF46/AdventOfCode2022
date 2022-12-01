1c9478e23fd048a811d78ad660de1346e681e2c3
TomF46 <t-d-f@live.co.uk>
2022-12-01 19:50:41 +0000

    Simplify as I was storing some information I didn't need.

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const events = require('events');
 const fs = require('fs');
 const readline = require('readline');
 
-let elfs = [];
-let currentElfNumber = 1;
+let calorieCounts = [];
 let currentElfCalories = 0;
 
 async function collectElfCalorieTotals() {
@@ -16,8 +15,8 @@ async function collectElfCalorieTotals() {
         rl.on('line', (line) => {
 
             if (isEmptyOrSpaces(line)) {
-                storeCurrentElf();
-                resetCurrentElf();
+                calorieCounts.push(currentElfCalories);
+                currentElfCalories = 0;
                 return;
             }
             currentElfCalories += Number(line);
@@ -25,7 +24,7 @@ async function collectElfCalorieTotals() {
 
         await events.once(rl, 'close');
 
-        storeCurrentElf(); // File doesn't end on an empty line so store final elf once finshed.
+        calorieCounts.push(currentElfCalories); // File doesn't end on an empty line so store final elf once finshed.
 
     } catch (err) {
         console.error(err);
@@ -36,31 +35,20 @@ function isEmptyOrSpaces(str) {
     return str === null || str.match(/^ *$/) !== null;
 }
 
-function resetCurrentElf() {
-    currentElfNumber++;
-    currentElfCalories = 0;
-};
-
-function storeCurrentElf() {
-    elfs.push({
-        number: currentElfNumber,
-        calories: currentElfCalories
-    });
-}
 
 function getElfWithMostCalories() {
-    return elfs.reduce((prev, current) => (prev.calories > current.calories) ? prev : current);
+    return calorieCounts.reduce((prev, current) => (prev > current) ? prev : current);
 }
 
 function getTopThreeCombinedCalories() {
-    return elfs.sort((e1, e2) => {
-        return e2.calories - e1.calories;
-    }).slice(0, 3).reduce((total, elf) => {
-        return total + elf.calories;
+    return calorieCounts.sort((e1, e2) => {
+        return e2 - e1;
+    }).slice(0, 3).reduce((total, current) => {
+        return total + current;
     }, 0);
 }
 
 collectElfCalorieTotals().then(() => {
-    console.log(`The elf with the most calories has ${getElfWithMostCalories().calories} calories`);
+    console.log(`The elf with the most calories has ${getElfWithMostCalories()} calories`);
     console.log(`The total amount of calories held by the top 3 elfs are ${getTopThreeCombinedCalories()} calories`);
 });
